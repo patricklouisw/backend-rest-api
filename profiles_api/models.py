7921acca80edebd4d63392f12fcf5f155e72ea66
Patrick Louis <patricklouisw@gmail.com>
2022-09-15 14:53:48 -0400

    Add a new APIView

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -29,6 +29,7 @@ class UserProfileManager(BaseUserManager):
         user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
+        user.save(using=self._db)
 
         return user
 
